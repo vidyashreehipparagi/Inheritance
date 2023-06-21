353664de4b1001f07b752d53b1d02c48562c4be9
vidyashreehipparagi <hipparagividyashree14@gmail.com>
2023-06-21 16:10:51 +0530

    Add project files.

---
+++ Inheritance/Employee.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class Employee
+    {
+        protected int id;
+        protected string name;
+        protected double bs, hra, ta, da, pf, gross;
+        private static int count;
+        public Employee(string name, double bs)
+        {
+            count++;
+            id = count;
+            this.name = name;
+            this.bs = bs;
+        }
+
+        public virtual void CalculateSalary()
+        {
+            hra = bs * 0.40;
+            ta = bs * 0.20;
+            da = bs * 0.15;
+            pf = bs * 0.12;
+            gross = (bs + hra + ta + da) - pf;
+        }
+        public override string ToString()
+        {
+            return $"id={id}, name={name}, gross ={gross}";
+        }
+    }
+
+    public class SalesManager : Employee
+    {
+        private double comm;
+        
+        public SalesManager(string name, double bs, double comm) : base(name, bs) //parametric constructor
+        {
+            this.comm = comm;
+
+        }
+        public override void CalculateSalary()
+        {
+            hra = bs * 0.40;
+            ta = bs * 0.20;
+            da = bs * 0.15;
+            pf = bs * 0.12;
+            gross = (bs + hra + ta + da + comm) - pf;
+        }
+        
+        public override string ToString()
+        {
+            return $"id={id}, name={name}, gross ={gross}";
+        }
+
+    }
+}
---
+++ Inheritance/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SalesManager salesManager = new SalesManager("Rohan", 28000, 3500);
+            salesManager.CalculateSalary();
+            Console.WriteLine(salesManager);// ToString() get called implicitly 
+
+            Employee employee = new Employee("Suraj", 23000);
+            employee.CalculateSalary();
+            Console.WriteLine(employee);
+
+        }
+    }
+}
